12487b780952b39800dd54a087bbaf312ef2c600
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-07-25 23:06:29 +0700

    ss

--- giftt/flutter_application_1/lib/main.dart
+++ giftt/flutter_application_1/lib/main.dart
@@ -18,38 +18,41 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() =>  _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class  _MyHomePageState extends State<MyHomePage> {
-int number= 0;
+class _MyHomePageState extends State<MyHomePage> {
+  int number = 0;
 
-  @override
-  Widget build(BuildContext context) {
+ @override
+ Widget build(BuildContext context){
+ List<Widget> data = [];
+  data.add(Text("กดปุ่มเพื่อเพิ่มจำนวน"));
+  data.add(
+            Text(
+              number.toString(),
+              style: TextStyle(fontSize: 60),
+            ),
+            );
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Hello Flutter"),
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:data,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-                Text("กดปุ่มเพื่อเพิ่มจำนวน "),
-                Text(
-                number.toString(),
-                style:  TextStyle(fontSize: 60),
-                ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed:() {
-            setState(() {
-              number=number+10;
-            });
-          },
-          child: Icon(Icons.add),
-        ),
-    );  
- }
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:addNumber, 
+        child:Icon(Icons.add),
+      ),
+    );
+  }
+void addNumber() {
+  setState(() {
+    number++;
+  });
+}
 }
