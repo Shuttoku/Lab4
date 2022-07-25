8e46f1faba440102f10dab2795a889b48df8b0fa
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-07-25 19:19:39 +0700

    ko

--- giftt/flutter_application_1/lib/main.dart
+++ giftt/flutter_application_1/lib/main.dart
@@ -8,18 +8,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: "My App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello Flutter ค่ะ"),
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Flutter"),
+        ),
+        body: Center(
+          child: Text(
+            "hi",
+            style: TextStyle(fontSize: 50, color: Colors.brown),
+          ),
+        ),
       ),
-      body: Text("สวัสดี"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.lime),
-  );
-
-
-   
-    
+      theme: ThemeData(primarySwatch: Colors.lime),
+    );
   }
 }
