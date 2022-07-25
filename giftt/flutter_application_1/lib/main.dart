df968e3fefb7ab5ec98373af78f1d7e060a3549d
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-07-25 18:15:57 +0700

    oo

--- giftt/flutter_application_1/lib/main.dart
+++ giftt/flutter_application_1/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
     title: "My App",
     home: Scaffold(
       appBar: AppBar(
@@ -11,5 +17,9 @@ void main() {
     ),
     theme: ThemeData(primarySwatch: Colors.lime),
   );
-  runApp(app);
+
+
+   
+    
+  }
 }
