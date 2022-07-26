01f1859c26ab83932dfeca9b07a367f477e54448
Shuttoku <108716387+Shuttoku@users.noreply.github.com>
2022-07-26 15:25:37 +0700

    pp

--- giftt/flutter_application_1/lib/main.dart
+++ giftt/flutter_application_1/lib/main.dart
@@ -23,39 +23,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int number = 0;
-  //กลุ่มข้อมูล
-  List<FoodMenu> menu = [
-    FoodMenu("กุ้งเผา","500",),
-    FoodMenu("ไก่ทอด","500",),
-    FoodMenu("ส้มตำ","60",),
-    FoodMenu("ส้ม","60",)
-  ];
 
+  //แสดงข้อมูล
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          //หัวเมนู
-          title: Text(
-            "เลือกเมนูอาหาร",
-            style: TextStyle(fontSize: 30),
+          appBar: AppBar(
+          title: Text("เลือก"),
           ),
-        ),
-        body: ListView.builder(
-            itemCount: menu.length,
-            itemBuilder: (BuildContext context, int index) {
-              FoodMenu food = menu[index];
-              return ListTile(
-                title: Text(
-                  food.name,
-                  style: TextStyle(fontSize: 20),
-                ),
-                subtitle: Text("ราคา" + food.price + "บาท"),
-                onTap: (){
-                  print("คุณเลือกอาหารชื่อว่าอะไร"+food.name);
-                },
-              );
-            }));
+          body: Column(children: [],)
+    );
   }
 }
+
+
+
